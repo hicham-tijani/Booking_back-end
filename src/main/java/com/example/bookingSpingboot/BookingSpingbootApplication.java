a0cbce5f4dbdb9239b707296e846a402776631c7
andre <d.castronunez@itsrizzoli.it>
2022-06-15 15:22:16 +0200

    first commit

---
+++ src/main/java/com/example/bookingSpingboot/BookingSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingSpingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSpingbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookingSpingboot/BookingSpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingSpingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSpingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
