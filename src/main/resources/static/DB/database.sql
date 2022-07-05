f7b5ccdca4ad0744bf1491a3d3aeb569205f7013
Hicham <99076537+hicham-tijani@users.noreply.github.com>
2022-07-05 15:00:40 +0200

    fix: db

--- src/main/resources/static/DB/database.sql
+++ src/main/resources/static/DB/database.sql
@@ -1,15 +1,4 @@
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE `users` (
   `id_users` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -24,26 +13,4 @@ ALTER TABLE `users`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking`
---
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`id`);
-
-CREATE TABLE `booking` (
-  `id_booking` int(11) NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `check_in` date NOT NULL,
-  `check_out` date NOT NULL,
-  `adulti` int NOT NULL,
-  `bambini` int NOT NULL,
-  `classe` varchar(25) NOT NULL,
-  `one_trip` boolean NOT NULL,
-  `roundtrip` boolean NOT NULL
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
